d4478cdfce318711b30cb1bf0463b186b0ead776
renzoram22 <69159900+renzoram22@users.noreply.github.com>
2021-06-27 22:18:05 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,29 +1,12 @@
 import React, { useState, useEffect } from 'react';
-import logo from './ico.ico';
 import './App.css';
 import 'semantic-ui-css/semantic.min.css'
-import { react } from '@babel/types';
 
 //Components
-import { Button, Icon } from 'semantic-ui-react';
 import Navbar from  './components/Navbar'
 import ItemListContainer from './components/Container/ItemListContainer'
-import {itemsData} from './components/Item/ItemsData'
 
 export default function App() {
-
-  const [items, setItems] = useState([]);
-  const itemsList = itemsData.map((item, indice) => `${item.name} ${indice}`)
-
-
-
-  useEffect(() => {
-    setTimeout(() => {
-        setItems(itemsData)
-        console.log(itemsList)
-    }, 2000);
-  });
-
 
   return (
     <>    
